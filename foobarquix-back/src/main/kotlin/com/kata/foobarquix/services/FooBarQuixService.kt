4fea327ca5ecc508ecc91cbed24b07c859498ed7
hamami.imed@gmail.com <hamami.imed@gmail.com>
2021-10-12 22:13:13 +0200

    GREEN

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -7,6 +7,7 @@ class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
         if(inputNumber%3==0) return "Foo";
+        if(inputNumber%5==0) return "Bar";
         return inputNumber.toString();
     }
 
