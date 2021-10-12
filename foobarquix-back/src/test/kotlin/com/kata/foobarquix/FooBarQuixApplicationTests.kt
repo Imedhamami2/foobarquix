aaefbe46733206bba0f4f0b6be0e06cade379c0a
hamami.imed@gmail.com <hamami.imed@gmail.com>
2021-10-12 21:28:47 +0200

    Red when should_return_input_as_string

--- foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
+++ foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
@@ -2,12 +2,20 @@ package com.kata.foobarquix
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import com.kata.foobarquix.services.FooBarQuixService
+import org.junit.jupiter.api.Assertions.assertEquals
 
 @SpringBootTest
 class FooBarQuixApplicationTests {
+
+	val fooBarQuixService = FooBarQuixService();
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	fun fooBarQuixNumber1shouldReturn1() {
+		assertEquals("1", fooBarQuixService.convertNumber(1))
+	}
 }
